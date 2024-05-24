b4ee24a0f460704fdeecb10d571e627e7a822e1e
Administrator <Administrator@PC-number>
2024-05-24 13:31:53 +0900

    家氏テストコミット

--- 8connect/src/main/java/model/dao/ConnectionManager.java
+++ 8connect/src/main/java/model/dao/ConnectionManager.java
@@ -1,5 +1,39 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionManager {
+	
+	/**
+	 * データベースURL
+	 */
+	private final static String URL = "jdbc:mysql://localhost:3306/8connectdb";
+
+	/**
+	 * ユーザ
+	 */
+	private final static String USER = "takoU";
+
+	/**
+	 * パスワード
+	 */
+	private final static String PASSWORD = "takoP";
+
+	/**
+	 * データベースへの接続を取得して返します。
+	 *
+	 * @return コネクション
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 */
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+
+		// JDBCドライバの読み込み
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		return DriverManager.getConnection(URL, USER, PASSWORD);
+
+	}
 
 }
